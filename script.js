5f120d355f4186b5ef5061df1cac20f9fcdef4ba
l.sosnowski <albercik33@icloud.com>
2022-06-11 15:17:47 +0200

    add2

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const menu = document.getElementById("Menu");
 const showMenuBtn = document.getElementById("ShowMenuBtn");
+const hideMenuBtn = document.getElementById("CloseMenuBtn");
 
 const showMenu = () => {
     console.log("Kliknięto button");
     // menu.style.display = "block"
+    // menu.style.transform = "translateX(0)";
+    menu.classList.add("menuOn");
+}
+
+const hideMenu = () => {
+    menu.classList.remove("menuOn");
 }
 
 showMenuBtn.addEventListener("click",showMenu);
+hideMenuBtn.addEventListener("click",hideMenu);
